c109fb2f318a8b7b5af72339f8a0ed2664549cc9
JohnMichaelOLavidez <Olaround31@gmail.com>
2023-03-07 02:00:54 -0500

    adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield"];{
+    //
+}
+    function  destructivelyAppendCat(name){
+        cats.push("Ralph");
+}
+    function destructivelyPrependCat(name){
+        cats.unshift("Bob");
+}
+    function destructivelyRemoveLastCat(name){
+        cats.pop("name");
+}
+    function destructivelyRemoveFirstCat(name){
+        cats.shift("name")
+}
+function appendCat(name){
+    const cats=["Milo","Otis","Garfield"];   
+    return appendCat = [...cats,"Broom"];
+ }
+ function prependCat(name){
+    const cats=["Milo","Otis","Garfield"];
+    return prependCat=["Arnold",...cats];
+ }
+ function removeFirstCat(name){
+    return cats.pop=["Otis","Garfield"]
+ }
+ function  removeLastCat(name){
+    return cats.shift=["Milo","Otis"]
+ }
